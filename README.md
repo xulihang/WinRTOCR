63b201ed375a93a0858f71b862f1493002435292
xulihang <xulihanghai@163.com>
2020-11-15 21:20:43 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # WinRTOCR
-.net desktop ocr console application which uses windows runtime ocr api
+
+A .net desktop ocr console application which uses windows runtime's Windows.Media.Ocr api.
+
+About calling Windows Runtime (WinRT) API from .net desktop console application:
+
+There are many posts about this, which is confusing.
+
+* [Create a Universal Windows Platform console app](https://docs.microsoft.com/zh-cn/windows/uwp/launch-resume/console-uwp). This creates a UWP app which needs installation.
+* [Call Windows Runtime APIs in desktop apps](https://docs.microsoft.com/zh-cn/windows/apps/desktop/modernize/desktop-to-uwp-enhance). I installed Microsoft.Windows.SDK.Contracts, but it didn't not work.
+* [C#/WinRT](https://github.com/microsoft/cswinrt/blob/master/README.md). Didn't work.
+* [How to use Windows.Devices.Geolocation API in a C# WinForm Win32 Desktop application in Windows 10?](https://social.msdn.microsoft.com/Forums/windows/en-US/40b4f86d-b6d5-430e-a235-7fc003dec4c4/how-to-use-windowsdevicesgeolocation-api-in-a-c-winform-win32-desktop-application-in-windows-10). This actually works. See the last post in 2019.
+
+
+
+
---
+++ WinRTOCR/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Globalization;
+using Windows.Graphics.Imaging;
+using Windows.Media.Ocr;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using System.Text.Json;
+
+namespace WinRTOCR
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            String imagePath="image.jpg";
+            String lang = "zh-Hans";
+            String output = "out.json";
+            if (args.Length != 0)
+            {
+                imagePath = args[0];
+                if (args.Length == 3)
+                {
+                    lang = args[1];
+                    output = args[2];
+                }
+            }
+            if (System.IO.File.Exists(imagePath))
+            {
+                await Write(imagePath, lang, output);
+            }
+            else
+            {
+                Console.WriteLine("Bad path");
+            }
+            //Print(imagePath,lang);
+            
+        }
+
+        private static async Task<OcrResult> GetResult(string imagePath, string lang)
+        {
+            StorageFile storageFile;
+            var path = Path.GetFullPath(imagePath);
+            storageFile = await StorageFile.GetFileFromPathAsync(path);
+            IRandomAccessStream randomAccessStream = await storageFile.OpenReadAsync();
+            BitmapDecoder decoder = await BitmapDecoder.CreateAsync(randomAccessStream);
+            SoftwareBitmap bitmap = await decoder.GetSoftwareBitmapAsync(BitmapPixelFormat.Bgra8, BitmapAlphaMode.Premultiplied);
+            Language language = new Language(lang);
+            OcrEngine ocrEngine = OcrEngine.TryCreateFromLanguage(language);
+            OcrResult ocrResult = await ocrEngine.RecognizeAsync(bitmap);
+            Console.Write(ocrResult.Text);
+            return ocrResult;
+        }
+
+        private static async Task<Object> Print(string imagePath, string lang)
+        {
+            OcrResult ocrResult = await GetResult(imagePath, lang);
+            Console.WriteLine(ocrResult.Text);
+            return "";
+        }
+
+        private static async Task<Object> Write(string imagePath, string lang, string output)
+        {
+            OcrResult ocrResult = await GetResult(imagePath, lang);
+            string jsonString;
+            jsonString = JsonSerializer.Serialize(ocrResult);
+            StreamWriter streamWriter= new StreamWriter(output);
+            streamWriter.Write(jsonString);
+            streamWriter.Dispose();
+            return "";
+        }
+    }
+}
